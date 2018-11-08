db84846f74e89c9149aa3089ebb26b185b8dc2ad
Kai Chang <kaijchang@gmail.com>
2018-11-07 22:04:01 -0800

    i have no idea

---
+++ Pyllurium/Elements.py
@@ -0,0 +1,16 @@
+class Element:
+    def __init__(self, name, Z):
+        self.name = name
+        self.Z = Z
+
+
+Hydrogen = Element('Hydrogen', 1)
+Helium = Element('Helium', 2)
+Lithium = Element('Lithium', 3)
+Beryllium = Element('Beryllium', 4)
+Boron = Element('Boron', 5)
+Carbon = Element('Carbon', 6)
+Nitrogen = Element('Nitrogen', 7)
+Oxygen = Element('Oxygen', 8)
+Flourine = Element('Flourine', 9)
+Neon = Element('Neon', 10)
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name='Pyllurium',
+    packages=['Pyllurium'],
+    version='0.1.0',
+    description='Python Implementation of Elemental Bonding',
+    author='Kai Chang',
+    author_email='kaijchang@gmail.com',
+    url='https://github.com/kajchang/Pyllurium',
+    license='MIT'
+)
